f42c942ece51a1a562e538add560aa31ea355ded
shashikanth <shashikanth.budhol@valuelabs.net>
2014-04-09 12:27:34 +0530

    Second Version

--- shashikanth/EmployeeLoginDetails/Test2.java
+++ shashikanth/EmployeeLoginDetails/Test2.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,13 +15,44 @@ public class Test2
 	public static Scanner sc=new Scanner(System.in);
 	public static void main(String[] args) throws IOException
 	{
-		String fileName=args[0];
+		String fileName=args[0],choice=null;
 	    List<Emp> elist=createEmpLoginList(fileName);
-	    for(Emp e:elist)
+	    do{
+	    System.out.println("Reports");
+	    System.out.println("1.No of Employees Present On a Particular Day");
+	    System.out.println("2.Total Working Hours of an Employee");
+	    System.out.println("choose any one of the above criteria:");
+	    int criteria=sc.nextInt();
+	    if(criteria==1)
 	    {
-	    	System.out.println(e.getId()+" "+e.isIn()+" "+e.getDate()+" "+e.getTime());
+	     calculateNoOfEmployeesPresent(elist);
 	    }
-	    calculateNoOfEmployeesPresent(elist);
+	    else
+	    {
+	      calculateWorkingHoursOfEmployee(elist);
+	    }
+	    System.out.println("Do u want to proceed(y/n):");
+	    choice=sc.next();
+	    }while(choice.equalsIgnoreCase("y"));
+	}
+	private static void calculateWorkingHoursOfEmployee(List<Emp> elist) 
+	{
+		System.out.println("Enter Emp Id:");
+		int empId=sc.nextInt();long inTime = 0,outTime = 0,diff = 0;Time time=Time.valueOf("11:00:00");
+		for(int i=0;i<elist.size();i++)
+		{
+			if((elist.get(i).getId()==empId) && (elist.get(i).isIn()) )
+			{
+				inTime+=elist.get(i).getTime().getTime();
+			}
+			if((elist.get(i).getId()==empId) && (!elist.get(i).isIn()))
+			{
+				outTime+=elist.get(i).getTime().getTime();
+			}
+		}
+		diff=Math.abs(outTime-inTime);
+		Time t=new Time(diff-time.getTime());
+		System.out.println("Total Working Hours Of Employee:"+t);
 	}
 	private static void calculateNoOfEmployeesPresent(List<Emp> elist) 
 	{
