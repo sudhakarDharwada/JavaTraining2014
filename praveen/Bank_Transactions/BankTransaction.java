07566298eb374e9fd7a82be717f3e74f96193dea
Praveen Ravipati <praveen.ravipati@valuelabs.net>
2014-04-15 12:12:16 +0530

    Bank_transaction v1.1

--- praveen/Bank_Transactions/BankTransaction.java
+++ praveen/Bank_Transactions/BankTransaction.java
@@ -1,34 +1,31 @@
 package com.snapfish.banktransaction.classes;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Hashtable;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.StringTokenizer;
-import java.util.Map.Entry;
 
 public class BankTransaction {
-	/*The Main method*/
+        /*The Main method*/
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
 		Hashtable<Integer, Double> UserAccounts = new Hashtable<Integer, Double>();
-		Scanner s = new Scanner(System.in);
-		/*Read the File*/
-		System.out.print("Enter The Input file path:");
-		String FilePath = s.next();
+		if (args.length != 1) {
+			System.err
+					.println("Invalid command line, exactly one argument required");
+			System.exit(1);
+		}
+		String FilePath = args[0];
 
 		BankTransaction bt = new BankTransaction();
-		try {
-			UserAccounts = bt.input(FilePath, UserAccounts);
-			bt.summary(UserAccounts);
-		}
-		catch (NumberFormatException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("File Read Error");
-		}
+		UserAccounts = bt.input(FilePath, UserAccounts);
+		bt.summary(UserAccounts);
 	}
-	/*This method diplays all Account details*/
+        /*This method diplays all Account details*/
 	public void summary(Hashtable<Integer, Double> userAccounts) {
 		System.out.println("\n\n\tSummary\n");
 		for (Entry<Integer, Double> entry : userAccounts.entrySet()) {
@@ -38,51 +35,71 @@ public class BankTransaction {
 					+ Amount);
 		}
 	}
-	/*This method read the file and perform creation of new account and transaction operations*/
+        /*This method read the file and perform creation of new account and transaction operations*/
 	public Hashtable<Integer, Double> input(String filePath,
-			Hashtable<Integer, Double> UserAccounts)
-			throws NumberFormatException, IOException {
-		BufferedReader input = new BufferedReader(new FileReader(filePath));
-		String str = null;
-		double bal = 0.0;
-		String arg[] = new String[3];
-		StringTokenizer sTokenizer = null;
-		while ((str = input.readLine()) != null) {
-			sTokenizer = new StringTokenizer(str, ",");
-			for (int i = 0; sTokenizer.hasMoreTokens(); i++) {
-				arg[i] = sTokenizer.nextToken();
-			}
-			if (arg[0] != null) {
-				int AccountNo = Integer.parseInt(arg[0]);
-				String transaction = arg[1];
-				double Amount = Double.parseDouble(arg[2]);
-				/*This if condition checks whether the account is exist or not*/
-				if (UserAccounts.containsKey(AccountNo)) {
-					/*Account is exist so, we perform transaction*/
-					UserAccounts = Transaction(UserAccounts, AccountNo, Amount,
-							transaction);
-				} else {
-					/*Account is not exist so we creating an Account*/
-					UserAccounts.put(AccountNo, bal);
-					/*we perform transaction*/
-					UserAccounts = Transaction(UserAccounts, AccountNo, Amount,
-							transaction);
+			Hashtable<Integer, Double> UserAccounts) {
+		BufferedReader input = null;
+		try {
+			input = new BufferedReader(new FileReader(filePath));
+			String str = null;
+			double bal = 0.0;
+			String arg[] = new String[3];
+			StringTokenizer sTokenizer = null;
+			while ((str = input.readLine()) != null) {
+				sTokenizer = new StringTokenizer(str, ",");
+				for (int i = 0; sTokenizer.hasMoreTokens(); i++) {
+					arg[i] = sTokenizer.nextToken();
+				}
+				if (arg[0] != null) {
+					int AccountNo = Integer.parseInt(arg[0]);
+					String transaction = arg[1];
+					double Amount = Double.parseDouble(arg[2]);
+					Double initialAmount = UserAccounts.get(AccountNo);
+                                        /*This if condition checks whether the account is exist or not*/
+					if (initialAmount != null) {
+                                                /*Account is exist so, we perform transaction*/
+						UserAccounts = transaction(UserAccounts, AccountNo,
+								Amount, transaction, initialAmount);
+					} else {
+                                                /*Account is not exist so we creating an Account*/
+                                                UserAccounts.put(AccountNo, bal);
+                                                /*we perform transaction*/
+					        UserAccounts = transaction(UserAccounts, AccountNo, Amount,
+							transaction, bal);
+
+					}
+					
 				}
 			}
+		} catch (NumberFormatException e) {
+			System.err.println("Input is not readables");
+		} catch (FileNotFoundException e) {
+			System.err.println("File is not found");
+		} catch (IOException e) {
+			System.err.println("File read error");
+		} finally {
+			try {
+				input.close();
+			} catch (IOException e) {
+				System.err.println("File Read Error");
+			}
 		}
-		input.close();
 		return UserAccounts;
 	}
-	/*This method perform transaction*/
-	public Hashtable<Integer, Double> Transaction(
+        /*This method perform transaction*/
+	public Hashtable<Integer, Double> transaction(
 			Hashtable<Integer, Double> UserAccounts, int AccountNo,
-			double amount, String transaction) {
+			double amount, String transaction, double initialAmount) {
 		if (transaction.equalsIgnoreCase("deposite")) {
-			UserAccounts.put(AccountNo, UserAccounts.get(AccountNo) + amount);
+			UserAccounts.put(AccountNo, initialAmount + amount);
 		} else if (transaction.equalsIgnoreCase("withdraw")) {
-			UserAccounts.put(AccountNo, UserAccounts.get(AccountNo) - amount);
+			UserAccounts.put(AccountNo, initialAmount - amount);
 		}
 		return UserAccounts;
 	}
 
 }
+
+
+/*input*/
+/*/home/praveen/Desktop/training/BankTransactions/src/com/snapfish/inputfiles/BankinputFile.txt*/
