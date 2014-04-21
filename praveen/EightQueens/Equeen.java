caa3f95ab2f028a1bc587bd51f80099e11b24d0f
Praveen Ravipati <ravipatipraveen77.45@gmail.com>
2014-04-21 16:51:47 +0530

    Equeen v1.3

--- praveen/EightQueens/Equeen.java
+++ praveen/EightQueens/Equeen.java
@@ -3,7 +3,7 @@ package com.eightqueen.test.programs;
 public class Equeen {
 	static int Matrix[] = new int[8];
 	static int sno = 0;
-        
+        /*This method find the Solution N Queens(8 Queens)*/
 	public void EqueenFinder() {
 		System.out.println("the solutions are");
 		for (int row = 0; row >= 0;) {
@@ -14,7 +14,7 @@ public class Equeen {
 				Matrix[row] = 0;
 				row--;
 				if (row < 0) {
-					System.out.println("\nThank Q");
+					System.out.println("\n\nThank Q");
 				} else {
 					Matrix[row] = Matrix[row] + 1;
 				}
@@ -31,20 +31,20 @@ public class Equeen {
 		}
 
 	}
-
-	public boolean isSafeToPalce(int pos) {
+        /*This Method checks whether at required position the element is insert or not*/
+	public boolean isSafeToPalce(int req_position) {
 		boolean safe = true;
-		for (int row = 0; row < pos; row++) {
-			if (Matrix[row] == Matrix[pos]
-					|| ((Math.abs(Matrix[row] - Matrix[pos])) == (Math.abs(row
-							- pos)))) {
+		for (int curr_row = 0; curr_row < req_position; curr_row++) {
+			if (Matrix[curr_row] == Matrix[req_position]
+					|| ((Math.abs(Matrix[curr_row] - Matrix[req_position])) == (Math.abs(curr_row
+							- req_position)))) {
 				safe = false;
 				return safe;
 			}
 		}
 		return safe;
 	}
-
+        /*This method print the all solution*/
 	public void out() {
 		sno++;
 		System.out.print("\n " + sno + ": ");
@@ -52,7 +52,7 @@ public class Equeen {
 			System.out.print("(" + row + "," + Matrix[row] + ")");
 		}
 	}
-
+        /*main method*/
 	public static void main(String[] args) {
 		Equeen eq = new Equeen();
 		eq.EqueenFinder();
