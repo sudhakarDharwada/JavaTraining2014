69dc9c6e33db215ee419dcbf5ab1dbca92c08841
Praveen Ravipati <ravipatipraveen77.45@gmail.com>
2014-04-21 17:48:22 +0530

    NQueen

---
+++ praveen/NQueen/Nqueen.java
@@ -0,0 +1,67 @@
+package com.eightqueen.test.programs;
+
+import java.util.Scanner;
+
+public class Nqueen {
+	static int Matrix[];
+	static int sno = 0;
+         /*This method find the Solution N Queens*/
+	public void EqueenFinder(int Nq) {
+		System.out.println("the solutions are");
+		for (int row = 0; row >= 0;) {
+			while ((Matrix[row] < Nq) && (!isSafeToPalce(row))) {
+				Matrix[row]++;
+			}
+			if (Matrix[row] > (Nq-1)) {
+				Matrix[row] = 0;
+				row--;
+				if (row < 0) {
+					System.out.println("\n\nThank Q");
+				} else {
+					Matrix[row] = Matrix[row] + 1;
+				}
+
+			} else {
+				if (row == (Nq-1)) {
+					out(Nq);
+					Matrix[row]++;
+				} else {
+					row++;
+					Matrix[row] = 0;
+				}
+			}
+		}
+		System.out.println("Total "+sno+" solutions");
+
+	}
+        /*This Method checks whether at required position the element is insert or not*/
+	public boolean isSafeToPalce(int req_position) {
+		boolean safe = true;
+		for (int curr_row = 0; curr_row < req_position; curr_row++) {
+			if (Matrix[curr_row] == Matrix[req_position] || ((Math.abs(Matrix[curr_row] - Matrix[req_position])) == (Math.abs(curr_row - req_position)))) {
+				safe = false;
+				return safe;
+			}
+		}
+		return safe;
+	}
+        /*This method print the all solution*/
+	public void out(int Nq) {
+		sno++;
+		System.out.print("\n " + sno + ": ");
+		for (int row = 0; row <Nq ; row++) {
+			System.out.print("(" + row + "," + Matrix[row] + ")");
+		}
+	}
+        /*main method*/
+	public static void main(String[] args) {
+		Nqueen eq = new Nqueen();
+		Scanner s=new Scanner(System.in);
+		System.out.print("\nEnter the Number of Queens:");
+		int No_of_Queens=s.nextInt();
+		Matrix=new int[No_of_Queens];
+		eq.EqueenFinder(No_of_Queens);
+	}
+
+}
+
