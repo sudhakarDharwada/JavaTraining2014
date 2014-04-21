6a48046f9810b8e902465f964fade28ac5dc6e89
Praveen Ravipati <ravipatipraveen77.45@gmail.com>
2014-04-21 16:35:40 +0530

    Equeen v1.2

--- praveen/EightQueens/Equeen.java
+++ praveen/EightQueens/Equeen.java
@@ -1,135 +1,60 @@
 package com.eightqueen.test.programs;
 
-
 public class Equeen {
 	static int Matrix[] = new int[8];
-	public static boolean yCondition(boolean ycon)
-	{
-		return ycon;
-	}
-
+	static int sno = 0;
+        
 	public void EqueenFinder() {
-		boolean ycon = false;
-		int i=0;
-		while(i<8)
-		{
-			int count=0;
-			for(int j=0;j<8;j++)
-			{
-				if(yCondition(ycon))
-				{
-					j=Matrix[i];
-				}
-				int condition=CheckCondition(Matrix, i, j);
-				
-				if (condition==8) {
-					count++;
-					break;
+		System.out.println("the solutions are");
+		for (int row = 0; row >= 0;) {
+			while ((Matrix[row] < 8) && (!isSafeToPalce(row))) {
+				Matrix[row]++;
+			}
+			if (Matrix[row] > 7) {
+				Matrix[row] = 0;
+				row--;
+				if (row < 0) {
+					System.out.println("\nThank Q");
+				} else {
+					Matrix[row] = Matrix[row] + 1;
 				}
 
-				ycon=false;
-			}
-			if(count==0)
-			{
-				Matrix[i]=0;
-				i=i-1; 
-				if(Matrix[i]>=7)
-				{
-					Matrix[i]=0;
-					i=i-1;
-					Matrix[i]=Matrix[i]+1;
-					ycon=true;
-				}
-				else {
-					Matrix[i]=Matrix[i]+1;
-					ycon=true;
-				}
 			} else {
-				ycon=false;
-				i++;
-			}
-
-		}
-		
-	}
-
-	public static int CheckCondition(int matix[],int x,int y) {
-		int result=0;
-			System.out.println("x:"+x+" y:"+y);
-			int count=0;
-			for(int i=x;i>=0;i--)
-			{
-				
-				System.out.println("\t i:"+i);
-				if(isSafeToPalce(i, matix[i], x,y))
-				{
-				
-				
-					count++;
-					if(Matrix[0]==0){
-						System.out.println("true");
-						Matrix[x]=y;
-					}
-					System.out.println("x1:"+i+" y1:"+Matrix[i]+" x2:"+x+" y2:"+y);
-				}
-				else {
-					System.out.println("roung");
-				}
-			}
-			if(x==0){
-				if(count==(x+1))
-				{
-				System.out.println("count:"+count);
-					Matrix[x]=y;
-				System.out.println("The Matrix at index:"+x+" and value:"+Matrix[x]);
-					result=8;
-				}
-			}
-			else {
-				if(count==7)
-				{
-					
-					System.out.println("count:"+count);
-					Matrix[x]=y;
-					System.out.println("The Matrix at index:"+x+" and value:"+Matrix[x]);
+				if (row == 7) {
 					out();
-					result=8;
-				}
-				else if(count==(x))
-				{
-					System.out.println("count:"+count);
-					Matrix[x]=y;
-				System.out.println("The Matrix at index:"+x+" and value:"+Matrix[x]);
-					result=8;
+					Matrix[row]++;
+				} else {
+					row++;
+					Matrix[row] = 0;
 				}
 			}
-		return result;
+		}
+
 	}
-	public static boolean isSafeToPalce(int x1, int y1, int x2, int y2) {
-		boolean safe = false;
-		if ((x1 == 0) && (x2 == 0)  && (y2 == 0)) {
-			safe = true;
-		} else if (y1 == y2) {
-			safe = false;
-		} else if (Math.abs((y2 - y1)) == Math.abs((x2 - x1))) {
-			safe = false;
-		} else if (x1 == x2) {
-			safe = false;
-		} else {
-			safe = true;
+
+	public boolean isSafeToPalce(int pos) {
+		boolean safe = true;
+		for (int row = 0; row < pos; row++) {
+			if (Matrix[row] == Matrix[pos]
+					|| ((Math.abs(Matrix[row] - Matrix[pos])) == (Math.abs(row
+							- pos)))) {
+				safe = false;
+				return safe;
+			}
 		}
 		return safe;
 	}
-	public static void out()
-	{
-		System.out.println("The places are");
-		for(int i=0;i<8;i++)
-		{
-			System.out.print("("+i+","+Matrix[i]+")\n");
+
+	public void out() {
+		sno++;
+		System.out.print("\n " + sno + ": ");
+		for (int row = 0; row < 8; row++) {
+			System.out.print("(" + row + "," + Matrix[row] + ")");
 		}
 	}
+
 	public static void main(String[] args) {
-		Equeen eq=new Equeen();
+		Equeen eq = new Equeen();
 		eq.EqueenFinder();
 	}
 
