24a6a4272b1fc855011b1e7336da0ed6f32621ad
Praveen Ravipati <ravipatipraveen77.45@gmail.com>
2014-04-29 14:05:25 +0530

    BanK Transaction Synchronization

--- praveen/Threads/Bank_Transactions/using Integer/Method I/program/BankTrans1.java
+++ praveen/Threads/Bank_Transactions/using Integer/Method I/program/BankTrans1.java
@@ -1,22 +1,21 @@
-package bankTransactionSampleProject.testcases;
+package com.snapfish.threads.mainclasses;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map.Entry;
-
-public class SynchronizationTest1 {
+import com.snapfish.threads.Threads.*;
+public class BankTrans1 {
 
 	public static void main(String[] args) {
-		SynchronizationTest1 st1=new SynchronizationTest1();
-		Hashtable<String, Double> userAccount = new Hashtable<String, Double>();
+		BankTrans1 st1=new BankTrans1();
+		Hashtable<Integer, Double> userAccount = new Hashtable<Integer, Double>();
 		if (args.length != 1) {
-			System.err
-					.println("Invalid command line, exactly one argument required");
-			System.exit(1);
-		}
-		String FilePath = args[0];
+                        System.err.println("Invalid command line, exactly one argument required");
+                        System.exit(1);
+                }
+                String FilePath = args[0];
 		File folder = new File(FilePath);
 		File[] listOfFiles = folder.listFiles();
 		List<Thread> list = new ArrayList<Thread>();
@@ -36,20 +35,19 @@ public class SynchronizationTest1 {
 			}
 		}
 		st1.summary(userAccount);
-		System.out.println("Main completed");
 		if (userAccount != null) {
 			userAccount = null;
 		}
 		
 	}
-	public void summary(Hashtable<String, Double> hashtable) {
+	public void summary(Hashtable<Integer, Double> hashtable) {
 		System.out.println("\n\n\tSummary\n");
-		for (Entry<String, Double> entry : hashtable.entrySet()) {
-			String Account = entry.getKey();
+		for (Entry<Integer, Double> entry : hashtable.entrySet()) {
+			Integer Account = entry.getKey();
 			double Amount = entry.getValue();
 			System.out.println("The Account Id:\t" + Account + "\t\tAmount:\t"
 					+ Amount);
 		}
 	}
-
 }
+
---
+++ praveen/Threads/Bank_Transactions/using String/Program/BankTrans1.java
@@ -0,0 +1,53 @@
+package com.snapfish.threads.mainclasses;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map.Entry;
+import com.snapfish.threads.Threads.*;
+public class BankTrans1 {
+
+	public static void main(String[] args) {
+		BankTrans1 st1=new BankTrans1();
+		Hashtable<String, Double> userAccount = new Hashtable<String, Double>();
+		if (args.length != 1) {
+                        System.err.println("Invalid command line, exactly one argument required");
+                        System.exit(1);
+                }
+                String FilePath = args[0];
+		File folder = new File(FilePath);
+		File[] listOfFiles = folder.listFiles();
+		List<Thread> list = new ArrayList<Thread>();
+
+		for (File file : listOfFiles) {
+			if (file.isFile()) {
+				MyThread t = new MyThread(file.getAbsolutePath(), userAccount);
+				t.start();
+				list.add(t);
+			}
+		}
+		for (Thread t : list) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		st1.summary(userAccount);
+		if (userAccount != null) {
+			userAccount = null;
+		}
+		
+	}
+	public void summary(Hashtable<String, Double> hashtable) {
+		System.out.println("\n\n\tSummary\n");
+		for (Entry<String, Double> entry : hashtable.entrySet()) {
+			String Account = entry.getKey();
+			double Amount = entry.getValue();
+			System.out.println("The Account Id:\t" + Account + "\t\tAmount:\t"
+					+ Amount);
+		}
+	}
+}
+
