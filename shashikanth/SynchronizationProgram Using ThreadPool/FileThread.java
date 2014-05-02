9d5d163907537a6ec80229fcffa6ca9c1a79dbc3
shashikanth <shashikanth.budhol@valuelabs.net>
2014-05-02 09:53:11 +0530

    Synchronization Program using Thread Pool

---
+++ shashikanth/SynchronizationProgram Using ThreadPool/FileThread.java
@@ -0,0 +1,27 @@
+package com.val.Threadpool;
+
+import java.io.IOException;
+
+class FileThread implements Runnable
+{
+	String file=null;
+	public FileThread(String file) 
+	{
+		this.file=file;
+	}
+	public void run()
+	{
+			try {
+				Transaction.processTransactions(file);	
+				
+			} catch (NumberFormatException e) {
+			
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+}
