aaf48ef2fa378a1a8c6c09127394f4b63f7bb770
Madhu10 <madhu.alishala@valuelabs.net>
2014-05-12 11:28:22 +0530

    TimerExample

---
+++ madhu/timerImplementation/BirthdayRemainder.java
@@ -0,0 +1,22 @@
+package org.thread.timerImplementation;
+
+import java.sql.Timestamp;
+
+public class BirthdayRemainder {
+
+	String name;
+	Timestamp timestamp;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+}
