f29777ea9236bd8634e15684a3fed89b640d92f1
kondamuri <chandrasekhar.kondamuri@valuelabs.net>
2014-05-21 19:35:38 +0530

    SchoolInfoUsingSaxParser

---
+++ sekhar/XMLSAXSchoolParser/Subjects.java
@@ -0,0 +1,34 @@
+package SchoolXml;
+
+public class Subjects {
+	private String sub1;
+	private String sub2;
+	private String sub3;
+	private String sub4;
+	public String getSub1() {
+		return sub1;
+	}
+	public void setSub1(String sub1) {
+		this.sub1 = sub1;
+	}
+	public String getSub2() {
+		return sub2;
+	}
+	public void setSub2(String sub2) {
+		this.sub2 = sub2;
+	}
+	public String getSub3() {
+		return sub3;
+	}
+	public void setSub3(String sub3) {
+		this.sub3 = sub3;
+	}
+	public String getSub4() {
+		return sub4;
+	}
+	public void setSub4(String sub4) {
+		this.sub4 = sub4;
+	}
+	
+
+}
