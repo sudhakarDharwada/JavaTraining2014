14a7acd998a8343cbf7070011cb24e456dc2e523
Praveen Ravipati <ravipatipraveen77.45@gmail.com>
2014-06-09 15:54:23 +0530

    added Login program

---
+++ praveen/WEB/Dynamic/Login using Filters and Listerns and Resource bundle/src/AppServletContext.java
@@ -0,0 +1,21 @@
+package com.vl.listner;
+
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+public class AppServletContext implements ServletContextListener {
+    public void contextInitialized(ServletContextEvent contextEvent) {
+       ServletContext context=contextEvent.getServletContext();
+       ResourceBundle rb = ResourceBundle.getBundle("com.vl.log.user");
+       System.out.println("init listerner");
+       context.setAttribute("prop", rb);
+    }
+    
+    public void contextDestroyed(ServletContextEvent contextEvent) {
+        System.out.println("distroyed listerner");
+        ResourceBundle.clearCache();
+    }
+	
+}
