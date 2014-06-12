3d8c208cae45e9f1381b06b037016f513c0e6f11
shashikanth <shashikanth.budhol@valuelabs.net>
2014-06-12 18:06:47 +0530

    WebApp Using Log4j

---
+++ shashikanth/J2EE/WebApp Using Log4j/src/com/val/Filters/AuthenticationFilter.java
@@ -0,0 +1,56 @@
+package com.val.Filters;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ResourceBundle;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+
+
+public class AuthenticationFilter implements Filter 
+{
+	ResourceBundle rb=null;
+	private static Logger logger=null;
+	public void destroy()
+	{
+	}
+	public void doFilter(ServletRequest req, ServletResponse resp,FilterChain chain) throws IOException, ServletException 
+	{
+		logger.debug("In Authentication Filter");
+		ServletContext context=req.getServletContext();
+		rb=(ResourceBundle) context.getAttribute("file");
+		HttpServletRequest request=(HttpServletRequest)req;
+		HttpServletResponse response=(HttpServletResponse)resp;
+		HttpSession session=request.getSession();
+		PrintWriter out=response.getWriter();
+		String username=req.getParameter("username");
+		String password=req.getParameter("pwd");
+		logger.info("UserName : "+username);
+		if(username!=null && username!="" && rb.containsKey(username))
+		{	
+			if(rb.getString(username).equals(password))
+			{
+				session.setAttribute("login", "true");
+				chain.doFilter(req, resp);
+			}
+		}
+		logger.debug("login failed");
+		request.getRequestDispatcher("Login.html").include(req, resp);
+		out.println("<center>Login Failed<center>");
+	}
+	public void init(FilterConfig filterconfig) throws ServletException 
+	{
+		logger=Logger.getLogger(AuthenticationFilter.class);
+	}
+}
