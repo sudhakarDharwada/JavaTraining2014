e242dc7e7439e8c3b5986612d7e4b46dc2e525bf
srinivas <srinivas.bavirisetty@valuelabs.net>
2014-08-26 11:37:04 +0530

    update fullcalender.js

--- Calender Application/WebContent/js/fullcalendar.js
+++ Calender Application/WebContent/js/fullcalendar.js
@@ -1,64 +1,39 @@
-        $('#calendar').ready(function()
-        	        {
-        	 
-        	
-        	            var date = new Date();
-        	            var d = date.getDate();
-        	            var m = date.getMonth();
-        	            var y = date.getFullYear();
+$('document').ready(function()
+{
+	var date = new Date();
+	var d = date.getDate();
+	var m = date.getMonth();
+	var y = date.getFullYear();
 
-        	            $( "#teams" ).click(function() {
-        	            	
-        	            	
-        	            	 var calendar = $('#calendar').fullCalendar(
-             	                    {
-             	                    	
-             	                        
-             	                        header:
-             	                        {
-             	                            left: 'prev,next today',
-             	                            center: 'title',
-             	                            right: 'month'
-             	                        },
-             	                        
-             	                        defaultView: 'month',
-             	                        
-             	                        
-             	                        selectable: true,
-             	                        selectHelper: true,
-             	                        
-             	                       
-             	                        
-             	                        events: [ ]
-             	                    });
-        	            });
-        	            
-        	            $('#calendar').ready(function(){
-        	            	
-       	            	 var calendar = $('#calendar').fullCalendar(
-            	                    {
-            	                    	
-            	                        
-            	                        header:
-            	                        {
-            	                            left: 'prev,next today',
-            	                            center: 'title',
-            	                            right: 'month'
-            	                        },
-            	                        
-            	                        defaultView: 'month',
-            	                        
-            	                        
-            	                        selectable: true,
-            	                        selectHelper: true,
-            	                        
-            	                       
-            	                        
-            	                        events: [ ]
-            	                    });
-       	            });
-        	            
-        	           	           
+	$( "#teams,#updateteam" ).click(function() {
+		checking();
+	});
+	
+	$('#calendar').ready(function(){
+		checking();
+	});
+});
+function checking(){
+	var calendar = $('#calendar').fullCalendar(
+			{
 
-        	        });
+				selectHelper: true,
 
+				header:
+				{
+					left: 'prev,next today',
+					center: 'title',
+					right: 'month'
+				},
+
+				defaultView: 'month',
+
+
+				selectable: true,
+				selectHelper: true,
+
+
+
+				events: [ ]
+			});
+}
