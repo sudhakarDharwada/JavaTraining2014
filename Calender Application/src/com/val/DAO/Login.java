20a9a41b12f65368c81bc03021541caf7317028f
rohit <rohit.gudesa@valuelabs.net>
2014-09-01 11:54:17 +0530

    login module updated

---
+++ Calender Application/src/com/val/DAO/Login.java
@@ -0,0 +1,21 @@
+package com.val.DAO;
+
+public class Login 
+{
+	int loginid;
+	String password;
+	
+	public int getLoginid() {
+		return loginid;
+	}
+	public void setLoginid(int loginid) {
+		this.loginid = loginid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
