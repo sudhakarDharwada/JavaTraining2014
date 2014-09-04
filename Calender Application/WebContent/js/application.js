beff9c7609bc9becae32911b4ef348424ab6d75c
shashikanth21 <shashikanth.budhol@valuelabs.net>
2014-09-04 11:16:47 +0530

    application.js and web.xml updated

--- Calender Application/WebContent/js/application.js
+++ Calender Application/WebContent/js/application.js
@@ -1,9 +1,20 @@
 $(function(){
-	var Event = Backbone.Model.extend();
+	var Event = Backbone.Model.extend({
+		defaults:{
+		title:"",
+		eventPlace:"",
+		start:""
+		},
+		urlRoot:"./event-rest/events"
+	});
 
 	var Events = Backbone.Collection.extend({
 		model: Event,
-		//url: 'events'
+		url: "./event-rest/events",
+		parse: function(response) {
+			console.log(" in parse");
+		    return response.calendarEvent;
+		}
 	});
 	var EventsView = Backbone.View.extend({
 		initialize: function(){
@@ -12,8 +23,7 @@ $(function(){
 			this.collection.bind('reset', this.addAll);
 			this.collection.bind('add', this.addOne);
 			this.collection.bind('change', this.change);
-			this.collection.bind('destroy', this.destroy);
-			//this.eventView = new EventView();
+			this.collection.bind('remove', this.destroy);
 		},
 		render: function() {
 			this.$el.fullCalendar({
@@ -29,7 +39,7 @@ $(function(){
 				select: this.select,
 				eventClick: this.eventClick,
 				eventDrop: this.eventDropOrResize,
-				eventResize: this.eventDropOrResize
+				eventResize: this.eventDropOrResize,
 			});
 		},
 		addAll: function() {
@@ -38,32 +48,43 @@ $(function(){
 		addOne: function(event) {
 			this.$el.fullCalendar('renderEvent', event.toJSON(),true);
 		},
-		select: function(startDate,endDate) {
+		select: function(startDate) {
 			this.eventView = new EventView();
 			this.eventView.collection = this.collection;
-			this.eventView.model = new Event({start: startDate,end:endDate});
+			console.log(startDate);
+			this.eventView.model = new Event({start: $.fullCalendar.formatDate(startDate,"yyyy-MM-dd")});
 			this.eventView.render();
 		},
 		eventClick: function(fcEvent) {
+			this.eventView = new EventView({collection:this.collection});
+			console.log(fcEvent);
+			console.log(fcEvent.id);
+			console.log(this.collection.get(fcEvent.id));
+			console.log(this.eventView.model);
 			this.eventView.model = this.collection.get(fcEvent.id);
 			this.eventView.render();
 		},
 		change: function(event) {
 //			Look up the underlying event in the calendar and update its details from the model
-			var fcEvent = this.el.fullCalendar('clientEvents', event.get('id'))[0];
+			console.log("at change start");
+			var fcEvent = this.$el.fullCalendar('clientEvents', event.get('id'))[0];
+			console.log("at change middle");
 			fcEvent.title = event.get('title');
 			fcEvent.eventPlace = event.get('eventPlace');
-			this.el.fullCalendar('updateEvent', fcEvent);
+			this.$el.fullCalendar('updateEvent', fcEvent);
+			console.log("total done");
 		},
 		eventDropOrResize: function(fcEvent) {
 //			Lookup the model that has the ID of the event and update its attributes
 			this.collection.get(fcEvent.id).save({start: fcEvent.start, end: fcEvent.end});
 		},
 		destroy: function(event) {
+			console.log(event.id);
 			this.$el.fullCalendar('removeEvents', event.id);
 		}
 	});
 
+	//var item = {title:"aaaaa",eventPlace:"bbbbb",start:""};
 	var EventView = Backbone.View.extend({
 		el: $('#eventDialog'),
 		initialize: function() {
@@ -90,14 +111,21 @@ $(function(){
 		},
 		save: function() {
 			this.model.set({'title': this.$('#title').val(), 'eventPlace': this.$('#place').val()});
-			this.collection.add(this.model);
-			$('#eventDialog').dialog('close');
-			/*if (this.model.isNew()) {
-				this.collection.create(this.model, {success: this.close});
+			console.log(this.model.isNew());
+		
+			if (this.model.isNew()) {
+				this.collection.create(this.model,{wait: true});
+				this.close();
 			} else {
 				this.model.save({}, {success: this.close});
-			}*/
+			}
 		},
+		handleData: function(data)
+		{
+			//document.write("updated --- id:"+data.id+" name:"+data.name+" description:" +data.description)
+			console.log("updated");
+
+		} ,
 		close: function() {
 			this.$el.dialog('close');
 		},
@@ -105,45 +133,49 @@ $(function(){
 			this.model.destroy({success: this.close});
 		}
 	});
-	
-	  var User=Backbone.Model.extend({
-	      defaults:{
-	        name:'',
-	        id:0,
-		email:'',
-	        phone:000,
-		designation:''
-	      },
-	      urlRoot:"./rest/profile"
-	    });
+
+	var User=Backbone.Model.extend({
+		defaults:{
+			name:'',
+			id:0,
+			email:'',
+			phone:000,
+			designation:''
+		},
+		urlRoot:"./profile-rest/profile"
+	});
 	var ProfileView=Backbone.View.extend({
-	  el:"#body",
-	  template:_.template($('#profile-template').html()),
-	  render:function(){
-		  this.$el.html('');
-		  this.$el.html(this.template(this.model.toJSON()));
-		  return this;
-	  }
+		el:"#body",
+		template:_.template($('#profile-template').html()),
+		render:function(){
+			this.$el.html('');
+			this.$el.html(this.template(this.model.toJSON()));
+			return this;
+		}
 	});
 	var AppRouter=Backbone.Router.extend({
-	  routes:{
-		  '':'home',
-		  'ProfileView':'showProfile'
-	  },
-	  showProfile:function(){
-		  var user=new User();
-		  user.fetch({success:function(response){
-			  var profileview=new ProfileView({model:response});
-			  profileview.render();
-		  }});
-	  },
-	  home:function(){
-		  window.events = new Events({title:'aaa',eventPlace:'home'});
-		  new EventsView({el: $("#body"), collection: events}).render();
-		  //events.fetch();
-	  }
+		routes:{
+			'':'home',
+			'ProfileView':'showProfile'
+		},
+		showProfile:function(){
+			var user=new User();
+			user.fetch({success:function(user){
+				var profileview=new ProfileView({model:user});
+				profileview.render();
+			}});
+		},
+		home:function(){
+			window.events = new Events();
+			new EventsView({el: $("#body"), collection: events}).render();
+			events.fetch({success:function(response){
+				console.log(response);
+				}
+			});
+		}
+
 	});
 	var approuter=new AppRouter();
 	Backbone.history.start();
-	
+
 });
